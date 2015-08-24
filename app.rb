bc7ff67e470466dc73e6c19321ac70420a151678
Justin Scott & Janice Dunn <student@epicodus.com>
2015-08-24 10:44:42 -0700

    Add tasks and lists databases using active record and the corresponding specs

--- app.rb
+++ app.rb
@@ -1,7 +1,33 @@
-require("sinatra")
-require("sinatra-contrib")
 require("sinatra/activerecord")
+require("sinatra")
+require("sinatra/reloader")
 require("./lib/list")
 require("./lib/task")
 require("pg")
 also_reload("/lib/**/*.rb")
+
+get("/") do
+  @tasks = Task.all()
+  erb(:index)
+end
+
+post("/tasks") do
+  description = params.fetch("description")
+  task = Task.new({:description => description})
+  task.save()
+  @tasks = Task.all()
+  erb(:index)
+end
+
+get("/tasks/:id/edit") do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i())
+  @task.update({:description => description})
+  @tasks = Task.all()
+  erb(:index)
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,21 @@
 ENV["RACK_ENV"] = 'test'
 
+require("sinatra/activerecord")
 require('rspec')
 require('capybara/rspec')
 require('pg')
 require('pry')
-require('./lib/list')
-require('./lib/task')
+require('list')
+require('task')
+
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Task.all().each() do |task|
+      task.destroy()
+    end
+    List.all().each() do |list|
+      list.destroy()
+    end
+  end
+end
